f13d21805d674360d7f17802fb43734240965e6b
unknown <eli..steiger@gmail.com>
2021-06-07 21:09:40 -0400

    foo

---
+++ src/components/MenuItem/index.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Card, CardContent, Typography } from "@material-ui/core";
+
+let styles = {
+  card: {
+    backgroundColor: "beige",
+    margin: "5px",
+    minWidth: "100px",
+  },
+  description: {
+    fontSize: 14,
+    float: "left",
+  },
+  price: {
+    float: "right",
+    marginBottom: "15px",
+  },
+};
+
+export default function MenuItem({ name, description, price, clickHandler }) {
+  return (
+    <Card style={styles.card} variant="outlined" key={name}>
+      <button data-value={name} onClick={clickHandler}>
+        X
+      </button>
+      <CardContent>
+        <Typography
+          variant="h5"
+          component="h2"
+          color="textSecondary"
+          gutterBottom
+        >
+          {name}
+        </Typography>
+        <Typography style={styles.description}>{description}</Typography>
+        <Typography style={styles.price} color="textSecondary">
+          {`$${price}`}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
---
+++ src/components/Panel/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import MenuItem from '../MenuItem'
+
+const styles = {
+    panel : {
+        minWidth: '100px',
+    }
+}
+
+export default function Panel({ items, clickHandler }) {
+    return (
+        <div style={styles.panel} className='container'>
+            { 
+                items.length ? items.map(item => MenuItem({...item, clickHandler})) :
+                <div>No Items In this Menu Panel</div>
+            }
+        </div>
+    )
+}
