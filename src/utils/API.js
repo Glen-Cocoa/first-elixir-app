1dabcba716e9fe87b4b98d81fe1fe3ade89b8d9e
unknown <eli..steiger@gmail.com>
2021-06-08 12:25:59 -0400

    remove extra dirs

--- src/utils/API.js
+++
@@ -1,8 +0,0 @@
-// This code is meant to serve as a mock fetch from an API.
-import getDummyData from "../tests/test-data";
-
-export const getMenu = new Promise(function (resolve) {
-  setTimeout(() => {
-    resolve(getDummyData());
-  }, 1000);
-});
--- src/utils/MenuContext.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const MenuContext = React.createContext({
-  items: [],
-});
-
-export default MenuContext;
